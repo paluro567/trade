642eb4f5f47afbb88f9325c7c3860baa14411055
Peter Luro <peterluro@Peters-Laptop.local>
2024-01-07 10:15:47 -0500

    updates

--- stop_script.sh
+++ stop_script.sh
@@ -4,5 +4,7 @@
 pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/test-12-10.py"
 pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/test-12-16.py"
 pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/realtime.py"
+pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/thirtyMin.py"
+
 
 
