e430b5f86e3525fb8da54ce5e78f16b0a2b19e10
Peter Luro <peterluro@Peters-Laptop.local>
2024-01-15 12:35:59 -0500

    updating stopscript

--- stop_script.sh
+++ stop_script.sh
@@ -1,10 +1,10 @@
 
 #!/bin/bash
 
-pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/test-12-10.py"
-pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/test-12-16.py"
-pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/realtime.py"
+pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/realtime.py
 pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/thirtyMin.py"
+pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/threeBar.py"
+pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/mainPositions.py"
 
 
 
