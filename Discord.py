58964b185046d89b4f0b6c36c3092a52b8e80874
Peter Luro <peterluro@Peters-Laptop.local>
2024-01-17 20:42:41 -0500

    test

--- Discord.py
+++ Discord.py
@@ -142,7 +142,7 @@ if __name__ == "__main__":
     from datetime import datetime
     import datetime
 
-    curr_date  =  datetime.datetime.utcnow().strftime('%Y-%m-%d')
+    curr_date  =  datetime.datetime.now().strftime('%Y-%m-%d')
     print("curr_date: ", curr_date)
     resistances, supports, retail, alarm_plays = get_briefing(curr_date)
     alarm_plays = [stock for stock in alarm_plays if ' ' not in stock]
