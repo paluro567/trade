afef39881f7cc4463db1a9770e7a5aabbf1cbea9
Peter Luro <peterluro@Peters-Laptop.local>
2024-01-22 08:40:46 -0500

    fixing stop script

--- stop_script.sh
+++ stop_script.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 
-pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/realtime.py
+pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/realtime.py"
 pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/thirtyMin.py"
 pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/threeBar.py"
 pkill -f "/home/peter_luro1/miniconda3/envs/tradeenv/bin/python /home/peter_luro1/trade/mainPositions.py"
