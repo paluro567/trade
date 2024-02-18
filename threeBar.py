bd9967c8667040165d6ff9fa9b06f00d5d660188
Peter Luro <peterluro@Peters-Laptop.local>
2024-02-18 15:54:37 -0500

    refactoring

--- threeBar.py
+++ threeBar.py
@@ -123,6 +123,11 @@ def nine_twenty_cross(df):
     return df.iloc[1]['ema_9']<df.iloc[1]['ema_20'] and df.iloc[0]['ema_9']>df.iloc[0]['ema_20']
 
 
+def crosses_below(df, threshold):
+    return (df.iloc[0]['close'] < threshold and df.iloc[0]['open'] > threshold) \
+           or (df.iloc[0]['close'] < threshold and df.iloc[1]['close'] > threshold)
+
+
 def monitor_bought_stock(ticker, qty, bought_price, support):
     
     while True:
@@ -130,11 +135,10 @@ def monitor_bought_stock(ticker, qty, bought_price, support):
         current_price = df.iloc[0]['close']
         time_stmp = df.iloc[0]['timestamp']
 
-        ema_cross = df.iloc[0]['open']>df.iloc[0]['ema_9'] and df.iloc[0]['close']<df.iloc[0]['ema_9'] \
-            or df.iloc[1]['close']>df.iloc[0]['ema_9'] and df.iloc[0]['close']<df.iloc[0]['ema_9']  # crosses below ema_9
-        
-        support_cross = df.iloc[0]['close']<support and df.iloc[0]['open']>support \
-            or df.iloc[0]['close']<support and df.iloc[1]['close']>support  # crosses below support
+        # is price weakening
+        ema_cross = crosses_below(df, 'ema_9', df.iloc[0]['ema_9'])
+        support_cross = crosses_below(df, 'close', support)
+
 
         percent_gain  =  ((current_price - bought_price) / bought_price) * 100
 
