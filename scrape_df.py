471c98a656a772458a1d43d5837190de6ee84054
Peter Luro <peterluro@Peters-Laptop.local>
2024-04-07 16:54:10 -0400

    adding scrape_df for testing

---
+++ scrape_df.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import time
+import requests
+from scrape_yahoo import scrape_stock_price
+from Discord import get_briefing
+from sms import text
+
+
+
+import pandas as pd
+import time
+import threading
+
+class Bar:
+    def __init__(self, open_price, close_price, percent_change, open_time):
+        self.open_price = open_price
+        self.close = close_price
+        self.percent_change = percent_change
+        self.open_time = open_time
+    def __str__(self):
+        return f"Open Price: {self.open_price}, Close Price: {self.close}, Percent Change: {self.percent_change}, Open Time: {self.open_time}"
+
+
+    
+
+
+# Example function to calculate percent change
+def calculate_percent_change(current_price, previous_price):
+    if previous_price is None:
+        return None
+    return ((current_price - previous_price) / previous_price) * 100
+def three_bar_breakout(cur_bars):
+    return cur_bars[-1].percent_change>5 \
+        and cur_bars[-2].percent_change<0 \
+            and cur_bars[-2].percent_change>5
+
+# Function to update the minute_interval_df
+def monitor_stock(ticker):
+    watch_bars=[]
+    while True:
+        current_time = pd.Timestamp.now()
+        try:
+            current_price = scrape_stock_price(ticker)
+        except Exception as e:
+            print(f"unable to scrape : {e}")
+
+        # create a new bar object
+        if len(watch_bars)<4 \
+            or (current_time - watch_bars[-1].open_time).total_seconds() >= 60:
+            new_bar= Bar(current_price, current_price, 0, current_time)
+            if len(watch_bars)>=4:
+                watch_bars.pop(0)
+            watch_bars.append(new_bar)
+        # update last bar
+        else:
+            update_bar=watch_bars[-1]
+            update_bar.close_price=current_price
+            update_bar.percent_change= calculate_percent_change(current_price, update_bar.open_price)
+            watch_bars[-1]=update_bar
+
+        if three_bar_breakout(ticker):
+            message =(f"scraped alert! {ticker}")
+            for bar in watch_bars:
+                message+="\n" + bar.__str__ + "\n"
+            text(message)
+
+
+if __name__=="__main__":
+    import datetime
+
+    from datetime import timezone
+    import pytz
+    import multiprocessing
+
+
+    # Get plays for current date
+    desired_timezone = 'America/New_York'
+    current_utc_time = datetime.datetime.now(timezone.utc)
+    current_local_time = current_utc_time.astimezone(pytz.timezone(desired_timezone))
+    curr_date_local = current_local_time.strftime('%Y-%m-%d')
+    alarm_plays, green_plays  =  get_briefing(curr_date_local) 
+
+    # start monitoring a stock
+    for play in alarm_plays:
+        process = multiprocessing.Process(target=monitor_stock, args=(play))
+        process.start()
+
+
--- scrape_yahoo.py
+++ scrape_yahoo.py
@@ -1,16 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
-
-headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
-stock='PLTR'
-url=f'https://finance.yahoo.com/quote/{stock}/'
-r = requests.get(url)
+import pandas as pd
+from datetime import datetime
 
 
-soup= BeautifulSoup(r.text, 'html.parser')
-print("status: ",r.status_code)
+def scrape_stock_price(stock):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    url = f'https://finance.yahoo.com/quote/{stock}/'
+    r = requests.get(url, headers=headers)
 
-price=soup.find('div', {'class':"D(ib) Mend(20px)"})
-print("price: ", price)
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-print("price text: ", price.text)
+    price = soup.find(class_="quote-header-section Cf Bxz(cb) Pos(r) Mb(5px) Bgc($lv2BgColor) Maw($maxModuleWidth) Miw($minGridWidth) smartphone_Miw(ini) Miw(ini)!--tab768 Miw(ini)!--tab1024 Mstart(a) Mend(a) Px(20px) smartphone_Pb(0px) smartphone_Mb(0px) smartphone_Z(11)")
+
+    resp_price_string = price.text.split("trend2W10W9M")[1]
+    string_price = ""
+    for char in resp_price_string:
+        if char.isdigit() or char == ".":
+            string_price += char
+        else:
+            break
+
+    return float(string_price)
+
+
+
+if __name__ == '__main__':
+ 
+    # Call the function to get the stock price and store it in the DataFrame
+    p = scrape_stock_price('pltr')
+
+    # Print the DataFrame
+    print(p)
