094e50d6ed2bf1e9f95528f7927233fcf5fdd111
Peter Luro <peterluro@Peters-Laptop.local>
2024-07-01 19:04:11 -0400

    new keys

--- alpaca.py
+++ alpaca.py
@@ -7,8 +7,8 @@ from record_day_trade import pdt_rule, record_trade
 
 # Alpaca API credentials
 BASE_URL = "https://api.alpaca.markets"
-API_KEY = "AK0N7FM1XU3EIZZAN6UR"
-API_SECRET = "rMPuDwYNDXLmQGXjhOe3BOluqdkwkMn2o1xPM01u"
+API_SECRET = "WPQhnKIKbs2XIBlwiYze9IoSDmIceMetVdzijvqY"
+API_KEY = 'AKNP95FFF31SO61LUUFA'
 
 # Initialize the Alpaca API
 API = tradeAPI.REST(API_KEY, API_SECRET, BASE_URL, api_version='v2')
@@ -16,8 +16,8 @@ API = tradeAPI.REST(API_KEY, API_SECRET, BASE_URL, api_version='v2')
 def get_current_timestamp():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# buy order
-def place_buy_order(ticker,qty):
+# Buy order
+def place_buy_order(ticker, qty):
     buy_order = API.submit_order(
         symbol=ticker,
         qty=qty,
@@ -25,10 +25,10 @@ def place_buy_order(ticker,qty):
         type='market',
         time_in_force='gtc'  # Good till canceled
     )
-    print(f"{get_current_timestamp()} - {ticker} -  buy order placed: {buy_order}")
+    print(f"{get_current_timestamp()} - {ticker} - Buy order placed: {buy_order}")
     return buy_order
 
-# stop loss order
+# Stop loss order
 def place_stop_loss_order(ticker, qty, cur_open):
     try:
         stop_loss_order = API.submit_order(
@@ -38,15 +38,15 @@ def place_stop_loss_order(ticker, qty, cur_open):
             type='stop',
             time_in_force='gtc',
             stop_price=cur_open
-        )  
-        print(f"{get_current_timestamp()} - {ticker} -  stop loss order placed: {stop_loss_order}")
+        )
+        print(f"{get_current_timestamp()} - {ticker} - Stop loss order placed: {stop_loss_order}")
         return stop_loss_order
     except Exception as e:
-        print(f"unable to place place_stop_loss_order=> {e}")
-        return 1
+        print(f"Unable to place stop loss order => {e}")
+        return None
 
+# Trailing stop order
 def place_trailing_stop_order(ticker, qty, trailing_pct):
-    # Place trailing stop sell order
     try:
         trailing_stop_order = API.submit_order(
             symbol=ticker,
@@ -59,53 +59,51 @@ def place_trailing_stop_order(ticker, qty, trailing_pct):
         print(f"{get_current_timestamp()} - {ticker} - Trailing stop sell order placed: {trailing_stop_order}")
         return trailing_stop_order
     except Exception as e:
-        print(f"unable to place trailing_stop_order=> {e}")
-        return 1
+        print(f"Unable to place trailing stop order => {e}")
+        return None
 
-
-# place buy and sell orders consecutively
+# Place buy and sell orders consecutively
 def try_orders(ticker, qty, cur_open, bars):
-    trailing_pct=10.0
+    trailing_pct = 10.0
     try:
-
         # Place market buy order
-        buy_order = place_buy_order(ticker,qty)
+        buy_order = place_buy_order(ticker, qty)
         
         # Wait for the buy order to be filled
         buy_order_filled = False
         while not buy_order_filled:
             buy_order_obj = API.get_order(buy_order.id)
             if buy_order_obj.status == 'filled':
-                buy_fill_time = datetime.now().date() # day date
+                buy_fill_time = datetime.now().date()  # Day date
                 buy_order_filled = True
             else:
                 time.sleep(1)  # Wait for 1 second before checking again
-        text(f"{get_current_timestamp()} - filled buy: {qty} shares of {ticker}")
+        text(f"{get_current_timestamp()} - Filled buy: {qty} shares of {ticker}")
 
-        # place sell orders
-        trailing_order=place_trailing_stop_order(ticker, qty,  trailing_pct)
-        stop_order=place_stop_loss_order(ticker, qty, cur_open)
+        # Place sell orders
+        trailing_order = place_trailing_stop_order(ticker, qty, trailing_pct)
+        stop_order = place_stop_loss_order(ticker, qty, cur_open)
 
-        # Wait for the sell_order to be filled
+        # Wait for the sell order to be filled
         order_filled = False
         while not order_filled:
-            # order objects
-            trailing_order_obj = API.get_order(trailing_order.id)
-            stop_order_obj = API.get_order(stop_order.id)
-            if trailing_order_obj.status == 'filled' or stop_order_obj.status == 'filled':
-            # if stop_order_obj.status == 'filled':
-                sell_fill_time = datetime.now().date() # day date
+            trailing_order_obj = trailing_order and API.get_order(trailing_order.id)
+            stop_order_obj = stop_order and API.get_order(stop_order.id)
+            if (trailing_order_obj and trailing_order_obj.status == 'filled') or (stop_order_obj and stop_order_obj.status == 'filled'):
+                sell_fill_time = datetime.now().date()  # Day date
                 order_filled = True
             else:
                 time.sleep(1)  # Wait for 1 second before checking again
 
-        text(f"{get_current_timestamp()} - filled Sell: {qty} shares of {ticker} \n\n {bars}")
+        text(f"{get_current_timestamp()} - Filled sell: {qty} shares of {ticker}\n\n{bars}")
 
-        if buy_fill_time==sell_fill_time:
+        if buy_fill_time == sell_fill_time:
             record_trade(ticker)
     except Exception as e:
         print(f"{get_current_timestamp()} - Unable to place orders for {ticker}: {e}")
 
-if __name__=='__main__':
-    place_buy_order('PLTR', 5)
-
+if __name__ == '__main__':
+    order_obj = place_trailing_stop_order('AMZN', 5, 5)
+    order_obj = API.get_order(order_obj.id)
+    print("order_obj: ", order_obj)
+    print("Order status: ", order_obj.status)
