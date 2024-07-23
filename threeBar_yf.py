1f28c3ab700e75fa785f213d45849d1e0a29802b
Peter Luro <peterluro@Peters-Laptop.local>
2024-07-23 18:58:26 -0400

    fixing 200 buy

--- threeBar_yf.py
+++ threeBar_yf.py
@@ -139,11 +139,11 @@ def check_play(ticker, play_type, priority, interval):
             print(f"Texting: {message}", flush=True)
             text(message)
             # place orders
-            if ticker not in BOUGHT_PLAYS and BOUGHT_AMT<200 and not pdt_rule() and is_before_noon_est():
-                print(f"placing {ticker} orders => {(200-BOUGHT_AMT)//close_price} shares", flush=True)
-                order_process = Process(target=try_orders, args=(ticker, (200-BOUGHT_AMT) // close_price, cur_open, three_bars)) #  submit orders
+            if len(BOUGHT_PLAYS) == 0  and not pdt_rule() and is_before_noon_est():
+                print(f"placing {ticker} orders => {(200)//close_price} shares", flush=True)
+                order_process = Process(target=try_orders, args=(ticker, 200 // close_price, cur_open, three_bars)) #  submit orders
                 order_process.start()
-                BOUGHT_AMT+= close_price*((200-BOUGHT_AMT)//close_price)
+                BOUGHT_AMT+= close_price*(200//close_price)
                 BOUGHT_PLAYS.append(ticker)
             TEXTED_PLAYS.append(ticker)
 
@@ -152,11 +152,11 @@ def check_play(ticker, play_type, priority, interval):
         if three_prior_pch>four_thresh and (two_prior_pch<0 or prior_pch<0) and cur_pch>four_thresh and ticker not in TEXTED_PLAYS:
             message = f"{play_type} - {priority} -  {ticker} 4 bar play\n Confirmation {round(cur_pch,2)}%\n test: {round(prior_pch,2)}%\n test: {round(two_prior_pch,2)}%\nignighting: {round(three_prior_pch,2)}%"
             text(message)
-            if ticker not in BOUGHT_PLAYS and BOUGHT_AMT<100 and not pdt_rule() and is_before_noon_est():
-                print(f"placing {ticker} orders => {(100-BOUGHT_AMT)//close_price} shares", flush=True)
-                order_process = Process(target=try_orders, args=(ticker, (100-BOUGHT_AMT) // close_price, cur_open, four_bars)) #  submit orders
+            if len(BOUGHT_PLAYS) == 0  and not pdt_rule() and is_before_noon_est():
+                print(f"placing {ticker} orders => {(200-BOUGHT_AMT)//close_price} shares", flush=True)
+                order_process = Process(target=try_orders, args=(ticker, 200 // close_price, cur_open, four_bars)) #  submit orders
                 order_process.start()
-                BOUGHT_AMT+= close_price*((100-BOUGHT_AMT)//close_price)
+                BOUGHT_AMT+= close_price*(200//close_price)
                 BOUGHT_PLAYS.append(ticker)
             TEXTED_PLAYS.append(ticker)
               
